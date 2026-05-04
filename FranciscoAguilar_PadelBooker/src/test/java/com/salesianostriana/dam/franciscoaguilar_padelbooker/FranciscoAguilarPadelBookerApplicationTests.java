236fd02ed1d8b65b252f25acd7270696633eb335
Paco Aguilar <aguilar.cifra25@triana.salesianos.edu>
2026-05-04 21:25:54 +0200

    Creación de proyecto

---
+++ FranciscoAguilar_PadelBooker/src/test/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/FranciscoAguilarPadelBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.franciscoaguilar_padelbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranciscoAguilarPadelBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
