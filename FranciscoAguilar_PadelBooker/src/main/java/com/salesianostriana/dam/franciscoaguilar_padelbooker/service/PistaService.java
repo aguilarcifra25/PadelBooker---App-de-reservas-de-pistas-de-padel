d367c6e75f68fffc9555942463423c6e03384235
francisco-aguilar <aguilar.cifra25@triana.salesianos.edu>
2026-05-06 10:42:21 +0200

    Inserción de Repository en los Service

--- FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/service/PistaService.java
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/service/PistaService.java
@@ -2,7 +2,14 @@ package com.salesianostriana.dam.franciscoaguilar_padelbooker.service;
 
 import org.springframework.stereotype.Service;
 
+import com.salesianostriana.dam.franciscoaguilar_padelbooker.repository.PistaRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class PistaService {
 
+	private final PistaRepository pistaRepository;	
+	
 }
--- FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/service/ReservaService.java
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/service/ReservaService.java
@@ -2,7 +2,14 @@ package com.salesianostriana.dam.franciscoaguilar_padelbooker.service;
 
 import org.springframework.stereotype.Service;
 
+import com.salesianostriana.dam.franciscoaguilar_padelbooker.repository.ReservaRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ReservaService {
 
+	private final ReservaRepository reservaRepository;
+	
 }
