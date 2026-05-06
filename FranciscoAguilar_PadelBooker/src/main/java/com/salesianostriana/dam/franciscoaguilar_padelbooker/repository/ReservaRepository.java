200caf3a907d5eddcb9add0960b0d48431f62424
francisco-aguilar <aguilar.cifra25@triana.salesianos.edu>
2026-05-06 10:38:55 +0200

    Creación de un Repository por entidad

---
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/repository/ReservaRepository.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.franciscoaguilar_padelbooker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salesianostriana.dam.franciscoaguilar_padelbooker.model.Reserva;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long>{
+
+}
---
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.franciscoaguilar_padelbooker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salesianostriana.dam.franciscoaguilar_padelbooker.model.Usuario;
+
+public interface UsuarioRepository  extends JpaRepository<Usuario, Long>{
+
+}
