11146d880da5716640b99a4a457833aba3fdf7f1
francisco-aguilar <aguilar.cifra25@triana.salesianos.edu>
2026-05-06 10:02:32 +0200

    Creación controllers de las entidades

---
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/controller/ReservaController.java
@@ -0,0 +1,12 @@
+package com.salesianostriana.dam.franciscoaguilar_padelbooker.controller;
+
+import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ReservaController {
+
+	
+	
+}
