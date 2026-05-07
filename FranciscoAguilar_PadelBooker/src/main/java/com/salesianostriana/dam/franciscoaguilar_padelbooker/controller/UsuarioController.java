a4747974cdbb8f3165be53730990994ae8e2a4db
Paco Aguilar <aguilar.cifra25@triana.salesianos.edu>
2026-05-07 19:51:00 +0200

    Adicion pagina principal al controller usuario

--- FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/controller/UsuarioController.java
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.salesianostriana.dam.franciscoaguilar_padelbooker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.salesianostriana.dam.franciscoaguilar_padelbooker.service.UsuarioService;
 
@@ -11,5 +13,13 @@ import lombok.RequiredArgsConstructor;
 public class UsuarioController {
 
 	private final UsuarioService usuarioService;
+	
+	@GetMapping("/")
+	public String paginaPrincipal (Model model) {
+		
+		
+		return "home";
+	}
+	
 	
 }
