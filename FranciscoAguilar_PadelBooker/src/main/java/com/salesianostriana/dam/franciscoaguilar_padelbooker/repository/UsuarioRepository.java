7053171a5fdcaf43c7b7c9f938d7926951657781
Paco Aguilar <aguilar.cifra25@triana.salesianos.edu>
2026-05-30 17:19:46 +0200

    feat:El admin puede filtrar usuarios

--- FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/repository/UsuarioRepository.java
+++ FranciscoAguilar_PadelBooker/src/main/java/com/salesianostriana/dam/franciscoaguilar_padelbooker/repository/UsuarioRepository.java
@@ -19,6 +19,23 @@ public interface UsuarioRepository  extends JpaRepository<Usuario, Long>{
 	@Query("SELECT u FROM Usuario u WHERE u.username != :username AND u.username != 'admin'")
 	List<Usuario> findAllExceptYouAndAdmin(@Param("username") String username);
 
+	@Query("SELECT u FROM Usuario u WHERE " +
+		       "u.username != 'admin' AND u.username != :logueado AND " +
+		       "(:nombre IS NULL OR LOWER(u.username) LIKE LOWER(CONCAT('%', :nombre, '%'))) AND " +
+		       "(:email IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%')))")
+		List<Usuario> buscarConFiltrosSinRol(@Param("logueado") String logueado,@Param("nombre") String nombre, @Param("email") String email
+		);
+
+	@Query("SELECT u FROM Usuario u WHERE " +
+	       "u.username != 'admin' AND u.username != :logueado AND " +
+	       "(:nombre IS NULL OR LOWER(u.username) LIKE LOWER(CONCAT('%', :nombre, '%'))) AND " +
+	       "(:email IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%'))) AND " +
+	       "u.rolUsuario = :rol")
+	List<Usuario> buscarConFiltrosConRol(@Param("logueado") String logueado, @Param("nombre") String nombre,
+											@Param("email") String email, @Param("rol") RolUsuario rol
+	);
+	
+	
 	Integer countByRolUsuario (RolUsuario rolUsuario);
 	
 }
